e296a3e0e4e7f2fd828ff543eabc401684630c02
ccino <lijfbest@gmail.com>
2024-06-26 09:48:08 +0800

    text bold

--- demo/src/main/java/com/ccino/demo/view/StyleActivity.kt
+++ demo/src/main/java/com/ccino/demo/view/StyleActivity.kt
@@ -1,17 +1,10 @@
 package com.ccino.demo.view
 
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
+import android.graphics.Typeface
 import android.os.Bundle
-import android.view.View.OnScrollChangeListener
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.core.content.ContextCompat
-import androidx.core.widget.NestedScrollView
-import com.blurview.RenderScriptBlur
 import com.ccino.demo.databinding.ActivityStyleBinding
-import com.ccino.demo.databinding.LayoutBlurViewBinding
 
 private const val TAG = "StyleActivity"
 
@@ -25,36 +18,7 @@ class StyleActivity : ComponentActivity() {
         enableEdgeToEdge()
         binding = ActivityStyleBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setupBlurView()
-//        binding.scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-//            binding.blurView.invalidate()
-//        }
-
-//        binding.blurView.postDelayed({
-//            binding.blurView.refreshBG(binding.coverView)
-//        }, 1000)
-//
-//
-//        binding.nestView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-//            binding.blurView.refreshBG(binding.coverView)
-//            binding
-//        }
+        binding.bold3.paint.isFakeBoldText = true
+        binding.bold4.setTypeface(Typeface.DEFAULT_BOLD)
     }
-
-//    private fun setupBlurView() {
-//        val radius = 20f
-//
-//        // 获取根视图
-//        val rootView = binding.backgroundImageView.parent as ViewGroup
-//
-//        // 设置背景，如果没有背景则使用透明背景
-//        val windowBackground: Drawable = binding.backgroundImageView.drawable
-//            ?: ContextCompat.getDrawable(this, android.R.color.transparent)!!
-//
-//        binding.blurView.setupWith(rootView)
-////            .setFrameClearDrawable(windowBackground)
-//            .setBlurRadius(radius)
-//            .setOverlayColor(-0x80000000)
-//
-//    }
 }
