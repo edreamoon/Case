eb1e4c011b4bd2346b38bd032b2e5313739f3725
ccino <lijfbest@gmail.com>
2024-08-27 16:15:21 +0800

    full screen：keyboard

---
+++ demo/src/main/java/com/ccino/demo/sdk/KeyboardActivity.kt
@@ -0,0 +1,20 @@
+package com.ccino.demo.sdk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.ccino.demo.R
+import com.ccino.demo.databinding.ActivityKeyboardBinding
+
+class KeyboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityKeyboardBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityKeyboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.content, KeyboardFragment())
+        }.commitNow()
+    }
+}
