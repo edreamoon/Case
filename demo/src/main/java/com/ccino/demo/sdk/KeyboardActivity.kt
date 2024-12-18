e76a0ab24aa18085d5ad67f62a8c12ca6ebf4873
ccino <lijfbest@gmail.com>
2024-12-18 14:03:27 +0800

    keyboard

--- demo/src/main/java/com/ccino/demo/sdk/KeyboardActivity.kt
+++ demo/src/main/java/com/ccino/demo/sdk/KeyboardActivity.kt
@@ -1,15 +1,25 @@
 package com.ccino.demo.sdk
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.ccino.demo.R
 import com.ccino.demo.databinding.ActivityKeyboardBinding
 
+/**
+ * 1. 非沉浸式状态栏时，
+ *  - 有可滑动的view时(scrollview/listView)并不需要去适配，键盘弹出时，会自动将布局上移，不会被键盘遮挡 （注释掉 enableEdgeToEdge）
+ *  - 无可滑动的view时，需要适配，添加 windowSoftInputMode="adjustResize" 即可
+ * 2. 沉浸式状态栏，
+ *  - 添加 windowSoftInputMode="adjustResize"，
+ *  - 布局添加 android:fitsSystemWindows="true"
+ *  - fitsSystemWindows view 需要去除顶部多余 padding， ViewCompat.setOnApplyWindowInsetsListener(rootView)
+ */
 class KeyboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityKeyboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         binding = ActivityKeyboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportFragmentManager.beginTransaction().apply {
--- demo/src/main/java/com/ccino/demo/sdk/KeyboardFragment.kt
+++ demo/src/main/java/com/ccino/demo/sdk/KeyboardFragment.kt
@@ -4,17 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import com.ccino.demo.R
+import com.ccino.demo.databinding.FragmentKeyboardBinding
+import com.ccino.demo.view.MoreAdapter
 
 
 class KeyboardFragment : Fragment() {
+    private lateinit var binding: FragmentKeyboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_keyboard, container, false)
+        binding = FragmentKeyboardBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+            val statusBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            view.setPadding(0, 0, 0, statusBars.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = MoreAdapter(view.context, false)
+        adapter.setData((1..20).map { it.toString() })
+        binding.listView.adapter = adapter
+    }
+
 }
